f577ab8f17968cb6cb2ed786504902f0b26a2ada
Tochukwu Ozo-Osiedo <tochukwuosiedo001@gmail.com>
2026-06-29 15:56:36 +0100

    hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,29 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-   <main>
-      home page
-   </main>
+    <main className="min-h-dvh">
+      <section className="h-dvh overflow-hidden relative">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+        >
+          <source src="/bg.mp4" type="video/mp4" />
+          Your browser does not support the video tag
+        </video>
+        <div className="h-dvh absolute bg-black/70 top-0 left-0 w-full flex justify-center">
+          <div className="w-2/3 lg:pt-20">
+            <h1 className="text-white text-4xl text-center"><span>API Connect</span> - The universal bridge for your data</h1>
+            <p className="text-white font-thin text-xl text-center">The seamless integration platform for modern developers. Effortlessly connect your apps, manage your endpoints, and power your digital products with clean, fast APIs</p>
+            <div className="text-white flex items-center justify-center">
+              <Link href={"/view"}>Explore APIs</Link>
+              <Link href={"/post"}>Publish Endpoints</Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,38 +1,104 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { GoArrowUpRight } from "react-icons/go";
 import { Theme } from "./Theme";
+import { HiOutlineMenuAlt4 } from "react-icons/hi";
+import { CgClose } from "react-icons/cg";
+import { useState } from "react";
 
-export default function Navbar () {
-    return (
-        <main className="flex items-center justify-between shadow-md max-md:px-3 md:px-10 py-2.5">
-            <Link href={"/"} className="flex items-center gap-0.5">
-                <Image
-                    src={"/logo.png"}
-                    alt="API Connect logo"
-                    width={500}
-                    height={500}
-                    className="w-8 h-8"
-                />
-                <p className="text-lg font-light">Connect</p>
-            </Link>
+interface NavItem {
+  name: string;
+  url: string;
+}
 
-            <div className="flex items-center gap-6">
-                <div className="flex items-center gap-6">
-                    <div className="">
-                        <Link href={"#"}>Home</Link>
-                        <span style={{backgroundColor:Theme.darkGreen}} className="h-0.5"></span>
-                    </div>
-                    <Link href={"#"}>About Us</Link>
-                    <Link href={"#"}>Post API</Link>
-                    <Link href={"#"}>View Endpoints</Link>
-                </div>
+export default function Navbar() {
 
-                <Link style={{backgroundColor: Theme.darkGreen}} href={"/signin"} className="flex gap-1 text-white px-4 py-1.5 rounded-sm items-center group">
-                    Get Started
-                    <GoArrowUpRight className="group-hover:translate-x-0.5 transition-all duration-200" />
-                </Link>
+    const [navOpen, setNavOpen] = useState(false)
+
+  const navItems: NavItem[] = [
+    {
+      name: "Home",
+      url: "/",
+    },
+    {
+      name: "About Us",
+      url: "/about",
+    },
+    {
+      name: "Post API",
+      url: "/post",
+    },
+    {
+      name: "View Endpoints",
+      url: "/view",
+    },
+  ];
+
+  return (
+    <main className="flex items-center justify-between shadow-md max-md:px-3 md:px-10 py-2.5 sticky top-0 z-50 bg-white">
+      <Link href={"/"} className="flex items-center gap-0.5 z-50">
+        <Image
+          src={"/logo.png"}
+          alt="API Connect logo"
+          width={500}
+          height={500}
+          className="w-8 h-8"
+        />
+        <p className="text-lg font-light max-md:hidden">Connect</p>
+      </Link>
+
+      <div className="flex items-center gap-6">
+        {/* desktop nav */}
+        <div className="flex items-center gap-6 max-lg:hidden">
+          {navItems.map((item, index) => (
+            <div key={index} className="group">
+              <Link href={item.url}>{item.name}</Link>
+              <div
+                style={{ backgroundColor: Theme.darkGreen }}
+                className="h-0.5 opacity-0 group-hover:opacity-100"
+              ></div>
             </div>
-        </main>
-    )
+          ))}
+        </div>
+
+        <Link
+          style={{ backgroundColor: Theme.darkGreen }}
+          href={"/signin"}
+          className="flex gap-1 text-white px-4 py-1.5 rounded-sm items-center group max-md:hidden z-50"
+        >
+          Get Started
+          <GoArrowUpRight className="group-hover:translate-x-0.5 transition-all duration-200" />
+        </Link>
+
+        {/* mobile and tablet nav */}
+        <div className={`h-dvh bg-white absolute top-0 left-0 w-full pt-20 items-center flex-col gap-10 lg:hidden ${navOpen ? "flex" : "hidden"}`}>
+          {navItems.map((item, index) => (
+            <div key={index} className="group">
+              <Link onClick={()=> setNavOpen(false)} href={item.url}>{item.name}</Link>
+              <div
+                style={{ backgroundColor: Theme.darkGreen }}
+                className="h-0.5 opacity-0 group-hover:opacity-100"
+              ></div>
+            </div>
+          ))}
+          <Link
+            onClick={()=> setNavOpen(false)}
+            style={{ backgroundColor: Theme.darkGreen }}
+            href={"/signin"}
+            className="flex gap-1 text-white px-4 py-1.5 rounded-sm items-center group md:hidden"
+          >
+            Get Started
+            <GoArrowUpRight className="group-hover:translate-x-0.5 transition-all duration-200" />
+          </Link>
+        </div>
+
+        <button onClick={()=> setNavOpen(!navOpen)} className="text-2xl lg:hidden z-50">
+            {
+                navOpen ? <CgClose /> : <HiOutlineMenuAlt4 />
+            }      
+        </button>
+      </div>
+    </main>
+  );
 }
